80d179cc28dd57a8703bdc61ae4b7fddef1b9274
Anmours <76721799+Anmours@users.noreply.github.com>
2022-04-30 23:53:02 +0800

    关机邮件通知模板

---
+++ mail_py/shutdown_mail.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+msg_from = 'xxxx@qq.com'  # 发送方邮箱        
+passwd = 'xx'             # 填入发送方邮箱的授权码        
+msg_to = 'xx@qq.com'      # 收件人邮箱       
+subject ='通知-已运行关机指令' # 主题        
+content = '您的NAS 由于8分钟时间已到，还未恢复市电，已运行关机指令' #内容   
+host = 'smtp.qq.com' #发信域名
+
+msg = MIMEText(content)        
+msg['Subject'] = subject        
+msg['From'] = msg_from
+msg['To'] = msg_to        
+
+s1 = smtplib.SMTP_SSL(host, 465)    #创建邮件服务
+s1.login(msg_from, passwd)   	 	#登录
+try:                      
+ s1.sendmail(msg_from, msg_to, msg.as_string())            
+ print("发送成功")        
+except:            
+ print("发送失败")        
+finally:            
+ s1.quit() 		#关闭服务
